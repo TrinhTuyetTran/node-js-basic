b83bcaafa42944287e8016b61e73ff54c0c286a8
thanhlong <longlieulinh111@gmail.com>
2024-03-08 11:33:28 +0700

    async

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ var cookieParser = require('cookie-parser');
 
 var infoRouter = require('./routes/info');
 var defaultRouter = require('./routes/default');
-var fundamentals = require('./routes/fundamentals')
+var fundamentals = require('./routes/fundamentals');
+var async = require('./routes/async-await');
 var app = express();
 
 app.use(express.json());
@@ -15,4 +16,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/info', infoRouter);
 app.use('*', defaultRouter);
 // app.use('*', fundamentals);
+// app.use('*',async);
 module.exports = app;
